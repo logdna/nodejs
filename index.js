40dc26f566b1d4253e25b4c770c120317581c816
respectus <muazs786@gmail.com>
2016-07-28 19:53:17 -0700

    Updated index.js from create to createLogger

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    create: require('./lib/logger').createLogger
+    createLogger: require('./lib/logger').createLogger
   , WinstonTransport: require('./lib/winston').WinstonTransport
 };
