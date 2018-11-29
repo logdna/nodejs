51fc67ed9c7ccded77710631d5e0b3ead39f8d0b
Adam King <rak@us.ibm.com>
2018-11-29 16:07:55 -0500

    Update nodejs dependencies and build
    - Update to LTS NodeJS 10
    - Update packages to select secure ones
    - ESLint the source
    - Eliminate 1m unit test exit delay
    - Generate code coverage report

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  *	LogDNA NPM Module Tests
  *		- Testing the LogDNA Node Code Library
@@ -32,16 +33,19 @@ module.exports = {
         , mac: 'C0:FF:EE:C0:FF:EE'
         , app: 'testing.log'
         , test: true
+        , logdna_url: 'http://localhost:1337'
     }
 };
 
 module.exports.memoryChecker = function(func) {
     var m1 = process.memoryUsage();
+
     console.time('    sendLogz');
-    func();
+    var p = func();
     console.timeEnd('    sendLogz');
     var m2 = process.memoryUsage();
     console.log('    Diff (MB) = rss: %j, heapTotal: %j, heapUsed: %j \n', (m2.rss - m1.rss) / 1000000, (m2.heapTotal - m1.heapTotal) / 1000000, (m2.heapUsed - m1.heapUsed) / 1000000);
+    return p;
 };
 
 module.exports.arraysEqual = function(a, b) {
